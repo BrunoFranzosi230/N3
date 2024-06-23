6666f96c01cc05cdfc0b51aac2080ef2feb7e27d
Bruno Franzosi <brunofranzosi230@hotmail.com>
2024-06-23 20:06:38 -0300

    Atualização

--- models/veiculo_model.js
+++ models/veiculo_model.js
@@ -9,5 +9,37 @@ const Veiculo = db.define('veiculo',{
         type: Sequelize.INTEGER,
         autoIncrement: true, 
     },
-    
-})
+    placa:{
+        type: Sequelize.STRING(10)
+        
+    },
+    modelo:{
+        type: Sequelize.STRING(100)
+    },
+    preco:{
+        type: Sequelize.DECIMAL(10,2)
+    },
+    proprietario_id:{
+        type: Sequelize.INTEGER
+    },
+    tipo_id:{
+        type:Sequelize.INTEGER
+    }
+
+
+},{
+    freezeTableName: true,
+    timestamps:false
+});
+
+Veiculo.associate = (models) => {
+    Veiculo.hasMany(models.Proprietario, 
+        {foreignKey: 'proprietario_id', as:  'Proprietario'})
+    Veiculo.hasMany(models.TipoVeiculo, 
+        {foreignKey: 'tipo_id', as: 'TipoVeiculo'})
+}
+
+Veiculo.belongsTo(Proprietario, {foreignKey: 'proprietario_id', allowNull: true})
+Veiculo.belongsTo(TipoVeiculo, {foreignKey: 'tipo_id', allowNull: true})
+
+export default Veiculo
